6acbd2b4182bddede2fc264c7aa55d3eeddf8df3
Dxrie <colinedric04@gmail.com>
2025-08-29 16:38:33 +0700

    create database schema for links and prefixes

---
+++ app/Models/Link.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Link extends Model
+{
+    protected $fillable = [
+        'target_url',
+        'unique_code',
+        'prefix_id',
+        'expired_at',
+    ];
+}
